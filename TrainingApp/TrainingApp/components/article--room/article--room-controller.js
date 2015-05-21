c01ea86fd575c1201a4bb1090692c853363fe526
mobiletonster <mobiletony@msn.com>
2015-05-21 09:42:12 -0600

    fixing some of rooms issues...unfinished code.

--- TrainingApp/TrainingApp/components/article--room/article--room-controller.js
+++ TrainingApp/TrainingApp/components/article--room/article--room-controller.js
@@ -1,8 +1,9 @@
-angular.module('class-scheduling').controller('roomController', ['$scope', '$q', 'roomService', 'trainingService', function($scope, $q, roomService, trainingService) {
+angular.module('class-scheduling').controller('roomController', ['$scope', '$q', 'articleRoomService', 'trainingService', function ($scope, $q, articleRoomService, trainingService) {
     'use strict';
-    $q.all([roomService.query().$promise, trainingService.query().$promise]).then(function (lists) {
+    $q.all([articleRoomService.query().$promise, trainingService.query().$promise]).then(function (lists) {
         var rooms     = lists[0],
             trainings = lists[1];
+        console.log(lists);
         $scope.rooms = rooms.map(function (room) {
             var rm = angular.copy(room);
             rm.trainings = trainings.filter(function (training) {
