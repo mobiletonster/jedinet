2bde007f5e9e4d2d246cd4930363da5225e3594e
mobiletonster <mobiletony@msn.com>
2015-06-08 13:09:21 -0600

    Updated somewhat

--- WebTemplate/src/WebTemplate/Controllers/AngularController.cs
+++ WebTemplate/src/WebTemplate/Controllers/AngularController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.IO;
 using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Hosting;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,11 +15,19 @@ namespace WebTemplate.Controllers
     {
         // GET: api/values
         [HttpGet]
-        //public HttpResponseMessage Get()
-        //{
-        //    var response = new HttpResponseMessage();
-        //    response.Content = new StreamContent(File.Open(AppDomain.CurrentDomain.BaseDirectory + "index.html", FileMode.Open));
-        //    return response;
-        //}
+        [Route("{url}")]
+        public IActionResult Get()
+        {
+            // return View("index.html");
+            return File("index.html","text/html");
+            
+            //var response = this.Context.Response;
+            
+
+            //var fileStream = System.IO.File.Open(AppDomain.CurrentDomain.BaseDirectory + "index.html", FileMode.Open);
+            //response.Body = fileStream;
+            //return response;
+        }
     }
 }
+
